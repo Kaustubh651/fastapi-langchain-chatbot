6316558fdb48671acb51bb14de26c6efe10c7454
Kaustubhd651 <92288567+Kaustubhd651@users.noreply.github.com>
2026-01-22 17:45:46 +0530

    Sprint-2: Add dynamic chat prompt templated with placeholder

--- app/prompts/chat_prompt.py
+++ app/prompts/chat_prompt.py
@@ -1,8 +1,20 @@
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage  
 
 
-chat_prompt = ChatPromptTemplate.from_messages([
-    ("system","You are a helpful {domain} expert."),
-    ("human","{user_input}")
-])
+def get_chat_prompt():
+    """
+    Return a dyanamic chat prompt templete for LLM interactions.
+    """
+    system_message = SystemMessage(
+        content=    ("You are a helpful AI assistant that provides accurate and concise information to users based on their queries."
+        "If you don't know the answer, respond with 'I don't know' instead of making up information.") 
+        
+    )
+
+    prompt = ChatPromptTemplate.from_messages([
+        system_message,
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human","{user_message}"),
+    ])
+    return prompt
